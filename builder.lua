16baa9dc8ff6e56f3f4480b02a54be05ff93bc68
twoThirdsHuman <axelwas07@gmail.com>
2025-03-21 23:01:17 +0100

    AAAAAA

--- builder.lua
+++ builder.lua
@@ -1,3 +1,20 @@
+local MAX_ITEM_SLOT = 1
+while true do
+    local status, err = pcall(function ()
+        turtle.getItemDetail(MAX_ITEM_SLOT)
+    end)
+    if err ~= nil then
+        MAX_ITEM_SLOT = MAX_ITEM_SLOT - 1
+        break
+    end
+    MAX_ITEM_SLOT = MAX_ITEM_SLOT + 1
+end
+
+function doAction(func) 
+    getStone()
+    errOnFalse(func)
+end
+
 function errOnFalse(func)
     local res = func()
     if res == false then
@@ -5,10 +22,41 @@ function errOnFalse(func)
     end
 end
 
+function getStone()
+    local block = turtle.getItemDetail()
+    if block ~= nil then
+        return
+    end
+
+    local hasStone = 0
+    for i = 1, MAX_ITEM_SLOT do
+        (function ()
+            local b = turtle.getItemDetail(i)
+    
+            if b == nil then
+                return
+            end
+    
+            local name = b["name"]
+            if string.find(name, "stone") then
+                hasStone = i
+            end
+        end)()
+    end
+
+    if hasStone == 0 then
+        error("could not find stone")
+    end
+
+    turtle.select(hasStone)
+    turtle.transferTo(1, 64)
+    turtle.select(1)
+end
+
 function doLine()
     for x = 1, 10 do
-        errOnFalse(turtle.back)
-        errOnFalse(turtle.place)
+        doAction(turtle.back)
+        doAction(turtle.place)
     end
 end
 
